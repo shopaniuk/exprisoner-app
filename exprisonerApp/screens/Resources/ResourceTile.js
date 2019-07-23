098b1739a11511f72d35143edca67b4a647b8621
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2019-07-22 21:44:44 -0700

    Wow

--- exprisonerApp/screens/Resources/ResourceTile.js
+++ exprisonerApp/screens/Resources/ResourceTile.js
@@ -8,8 +8,8 @@ const ResourceTile = (props) => {
     // Possible solution. Webhosting the images from a backend API that serves resources to display
     return(
         <View style = {{backgroundColor: '#D3D3D3', alignSelf: 'flex-start', margin: 3}}>
-            <Image source={require(props.resource.image)}/>
-            <Text>{props.resource.text}</Text>
+            {/* <Image source={require(props.resource.image)}/>
+            <Text>{props.resource.text}</Text> */}
         </View>
     );
 }
